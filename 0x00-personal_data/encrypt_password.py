484f56eb1daf50caa9beb1b1fd06d138cf208a4d
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-05-23 18:11:29 +0300

    Task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,17 @@ def hash_password(password: str) -> bytes:
         bytes: The salted, hashed password.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against a given hashed password using bcrypt.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain text password to validate.
+
+    Returns:
+        bool: True if the password matches the hashed password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
